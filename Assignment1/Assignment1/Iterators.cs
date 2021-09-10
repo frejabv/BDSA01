a8ef2924a64ab9520fc99a38641d6af2fab32c29
frev <frev@itu.dk>
2021-09-10 13:11:01 +0200

    Implement iterator method

--- Assignment1/Assignment1/Iterators.cs
+++ Assignment1/Assignment1/Iterators.cs
@@ -7,7 +7,13 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (var list in items)
+            {
+                foreach (var item in list) 
+                {
+                    yield return item;
+                }
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
