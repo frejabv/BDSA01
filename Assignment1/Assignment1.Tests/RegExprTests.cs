9c4762023320b4e0ffc2b5d740bf53dc4155659d
frev <frev@itu.dk>
2021-09-10 13:54:46 +0200

    Implement splitline test

--- Assignment1/Assignment1.Tests/RegExprTests.cs
+++ Assignment1/Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void given2LinesReturns6words() {
+            // Arrange
+            var firstLine = "jeg er mikki";
+            var secondLine = "mikki er jeg";
+            var input = new List<string> {firstLine, secondLine};
+
+            var expectedOutput = new List<string> {"jeg", "er", "mikki", "mikki", "er", "jeg"};
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
     }
 }
