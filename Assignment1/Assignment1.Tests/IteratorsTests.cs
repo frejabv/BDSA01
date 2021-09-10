1d6d320834acdca07acd4197daac81b60dd4fe81
frev <frev@itu.dk>
2021-09-10 13:03:44 +0200

    Implement iterator test

--- Assignment1/Assignment1.Tests/IteratorsTests.cs
+++ Assignment1/Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,26 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void Flatten_get2by2T_return4T() 
+        {
+            // Arrange
+            var list1 = new List<char>{'T','T'};
+            var list2 = new List<char>{'T','T'};
+            var lists = new List<List<char>>{list1,list2};
+
+            var expectedList = new List<char>{'T','T','T','T'};
+
+            // Act
+            var output = Iterators.Flatten(lists);
+
+            // Assert
+            Assert.Equal(expectedList, output);
+        }
     }
 }
