6d402b28606776a5877643beb91b07facc2b139e
frev <frev@itu.dk>
2021-09-10 13:33:13 +0200

    Implement filter test

--- Assignment1/Assignment1.Tests/IteratorsTests.cs
+++ Assignment1/Assignment1.Tests/IteratorsTests.cs
@@ -22,5 +22,25 @@ namespace Assignment1.Tests
             // Assert
             Assert.Equal(expectedList, output);
         }
+
+        [Fact]
+        public void Filter_get_1_2_3_4_andEvenPredicateReturn_2_4()
+        {
+            // Arrange
+            var input = new List<int>{1,2,3,4};
+            var expectedOutput = new List<int>{2,4};
+            Predicate<int> even = Even;
+
+            // Act
+            var output = Iterators.Filter(input, Even);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        } 
+
+        public static bool Even(int i)
+        {
+            return i % 2 == 0;
+        }
     }
 }
