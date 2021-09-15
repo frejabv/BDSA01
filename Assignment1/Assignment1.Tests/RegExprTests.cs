3b0c2b13f0956a1dc76f448d50aabf82741bcb4d
kaep <60853973+kaep@users.noreply.github.com>
2021-09-15 14:33:42 +0200

    Iterator (#6)
    * Implement iterator test
    
    * Implement iterator method
    
    * Implement filter test
    
    * Implement filter methods
    
    * Implement splitline test
    
    * Implement splitline method
    
    * Chaos solution
    
    * Implement and test Resolutions()
    
    * Implement and test first part of InnerText. Attempt to implement and test second part
    
    * Innertext regex replace (#2)
    
    * Modify innertext to remove all html tags
    
    * Fix build
    
    Co-authored-by: frev <frev@itu.dk>
    
    * Added more tests for Flatten and Filter (#5)
    
    Co-authored-by: Gustav Samuel Bonde Holmgren <guho@itu.dk>
    
    * Resolution tests (#3)
    
    * Add more resolution tests and change parameter in method
    
    Co-authored-by: frev <frev@itu.dk>
    
    * Feat/further testing innertext (#4)
    
    * write more unit tests for InnerText method + make correction to InnerText based on test results
    
    * Add new unit test for SplitLine
    
    * Implement more tests for SplitLine + change regex in SplitLine as a result of testing
    
    * delete comment
    
    * remove redundant using-statements
    
    Co-authored-by: kaeppen <seho@itu.dk>
    Co-authored-by: frejabv <90038232+frejabv@users.noreply.github.com>
    
    Co-authored-by: frev <frev@itu.dk>
    Co-authored-by: seho <seho@itu.dk>
    Co-authored-by: frejabv <90038232+frejabv@users.noreply.github.com>
    Co-authored-by: Gustavholm2 <49881816+Gustavholm2@users.noreply.github.com>
    Co-authored-by: Gustav Samuel Bonde Holmgren <guho@itu.dk>

--- Assignment1/Assignment1.Tests/RegExprTests.cs
+++ Assignment1/Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,130 @@
+using System.Collections;
+using System; 
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_given2LinesReturns6words() {
+            // Arrange
+            var firstLine = "jeg er mikki";
+            var secondLine = "mikki er jeg";
+            var input = new List<string> {firstLine, secondLine};
+
+            var expectedOutput = new List<string> {"jeg", "er", "mikki", "mikki", "er", "jeg"};
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void SplitLine_given_three_lines_returns_11_single_words() 
+        {
+            var input = new List<string>{"In todays world", "it is not", "uncommon to love Harry Potter"};
+            var expectedOutput = new List<string> {"In", "todays", "world", "it", "is", "not", "uncommon", "to", "love", "Harry", "Potter"};
+            Assert.Equal(expectedOutput, RegExpr.SplitLine(input));
+        }
+
+        [Fact]
+        public void SplitLine_given_two_lines_where_one_is_empty_returns_1_word() 
+        {
+            var input = new List<string>{"Alohomora", ""};
+            var expectedOutput = new List<string> {"Alohomora"}; 
+            Assert.Equal(expectedOutput, RegExpr.SplitLine(input));
+        }
+    
+        [Fact] 
+        public void Resolution_given_4_strings_of_8_resolutions_returns_8_tuples()
+        {
+            //Arrange 
+            var input = new List<string>{"1920x1080", "1024x768, 800x600, 640x480", "320x200, 320x240, 800x600", "1280x960"};
+            var expectedOutput = new List<(int, int)>{(1920, 1080), (1024, 768), (800, 600), (640, 480), (320, 200), (320, 240), (800, 600), (1280, 960)};
+
+            //Act 
+            var output = RegExpr.Resolution(input);
+
+            //Assert
+            Assert.Equal(expectedOutput, output); 
+        }
+
+        [Fact] 
+        public void Resolution_given_3_strings_of_5_resolutions_returns_5_tuples()
+        {
+            //Arrange 
+            var input = new List<string>{"2784x274", "28473x9274", "284x2422, 9472x243,1957x2922"};
+            var expectedOutput = new List<(int, int)>{(2784,274), (28473,9274), (284,2422), (9472,243),(1957,2922)};
+
+            //Act 
+            var output = RegExpr.Resolution(input);
+
+            //Assert
+            Assert.Equal(expectedOutput, output); 
+        }
+
+        [Fact] 
+        public void Resolution_given_2_wrong_input_format_returns_3_tuples()
+        {
+            //Arrange 
+            var input = new List<string>{"2784xxxx274", "\n284x", "284x2422,9472x243,,,,,,,,,1957x2922"};
+            var expectedOutput = new List<(int, int)>{(284,2422), (9472,243), (1957,2922)};
+
+            //Act 
+            var output = RegExpr.Resolution(input);
+
+            //Assert
+            Assert.Equal(expectedOutput, output); 
+        }
+
+    
+        [Fact]
+        public void InnerText_given_sample_string_from_assignment_returns_only_content_of_a_tags()
+        {
+            //Arrange
+            var input = "<div><p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+            var expectedOutput = new List<string>{"theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings"};
+
+            //Act
+            var output = RegExpr.InnerText(input, "a");
+
+
+            //Assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+
+        [Fact]
+        public void InnerText_given_nested_sample_html_returns_all_innertext_without_tags()
+        {
+            //Arrange
+            var input = "<div><p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p></div>";
+            var expectedOutput = new List<string>{"The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."};
+
+            //Act
+            var output = RegExpr.InnerText(input, "p");
+
+            //Assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Theory]
+        [InlineData("<div><p>The phrase <i>regular <b>expressions</b></i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p></div>", "p", "The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to.")]
+        [InlineData("<html>hej <header>jeg <p><i>er</i></p></header></html>","html","hej jeg er")]
+        [InlineData("<html><header><p><i>hej</i></p></header></html>","html","hej")] 
+        public void InnerText_given_html_with_multiple_nestings_returns_all_innertext_without_tags(string html, string tag, string output)
+        {   
+            var expectedOutput = new List<string>{output};
+
+            Assert.Equal(expectedOutput, RegExpr.InnerText(html, tag));
+        }
+    
+
+
+
     }
 }
--- Assignment1/Assignment1/RegExpr.cs
+++ Assignment1/Assignment1/RegExpr.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
 
@@ -7,17 +10,37 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (var line in lines)
+            {
+                foreach (var word in Regex.Matches(line, @"\w+"))
+                {
+                    yield return word.ToString(); 
+                } 
+            }
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) 
         {
-            throw new NotImplementedException();
+                foreach (var line in resolutions)
+                { 
+                    foreach (Match match in Regex.Matches(line, @"(?<horizontal>\d+)"+@"(?:x)"+@"(?<vertical>\d+)"))
+                    {   
+                        var width = Int32.Parse(match.Groups["horizontal"].Value);
+                        var height = Int32.Parse(match.Groups["vertical"].Value); 
+                        yield return (width, height);
+                    } 
+                }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+            var temp = "(?<tag>"+tag+")";   
+            string pattern = @"(?:<"+temp+".*?)(?<=>)(?<text>.*?)(?=</\\k<tag>)";  
+            foreach (Match match in Regex.Matches(html, pattern)) 
+            {
+                yield return Regex.Replace(match.Groups["text"].Value, @"(<.+?>)", "");
+            }
         }
+    
     }
 }
